086b56bf6fab6e0c613386f9e920d1aad2aa6458
Andresa43 <85956453+Andresa43@users.noreply.github.com>
2022-07-06 16:00:03 -0300

    new code

---
+++ exercise22.py
@@ -0,0 +1,14 @@
+nomeCompleto = str(input('Digite o seu nome completo: '))
+
+maius = nomeCompleto.upper()
+minus = nomeCompleto.lower()
+letterNoSpace = nomeCompleto.strip()
+letter = len(letterNoSpace) 
+letterSplit = nomeCompleto.split()
+firtletter = len(letterSplit[0])
+
+print(maius)
+print(minus)
+print(letter)
+print(firtletter)
+
