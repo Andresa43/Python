e014a848a1f9e32aa3cb858223da3646587804bb
Andresa43 <85956453+Andresa43@users.noreply.github.com>
2022-07-06 12:33:25 -0300

    improved the code

--- exercise17.py
+++ exercise17.py
@@ -1,8 +1,13 @@
-from math import sqrt
+from math import sqrt, hypot
 
 catop = float(input('Digite o cateto oposto da hotenusa: '))
 catadj = float(input('Digite o cateto adjacente da hotenusa: '))
 
-hipotenusa = sqrt(catop ** 2 + catadj ** 2)
+hipotenusa = hypot(catop,catadj )
 
 print('O comprimento da hipotenusa é: {:.2f}'.format(hipotenusa))
+
+# hipotenusa = sqrt(catop ** 2 + catadj ** 2)
+
+# print('O comprimento da hipotenusa é: {:.2f}'.format(hipotenusa))
+
