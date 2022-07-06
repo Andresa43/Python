792ee6afb993da031367a66f3e9f7975c957f9dc
Andresa43 <85956453+Andresa43@users.noreply.github.com>
2022-07-06 12:51:47 -0300

    improved the code

--- exercise20.py
+++ exercise20.py
@@ -1,9 +1,9 @@
-from msilib import sequence
-import random 
+from random import sample
+
 
 nome1 = str(input('Digite o primeiro nome: '))
 nome2 = str(input('Digite o segundo nome: '))
 nome3 = str(input('Digite o terceiro nome: '))
 nome4 = str(input('Digite o quarto nome: '))
 
-print('Ordem sorteada: {}'.format(random.sample([nome1, nome2, nome3, nome4], 4)))
+print('Ordem sorteada: {}'.format(sample([nome1, nome2, nome3, nome4], 4)))
