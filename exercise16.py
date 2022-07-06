525003557b7ae7deef77c5eb8ba8bcdd2c221a6b
Andresa43 <85956453+Andresa43@users.noreply.github.com>
2022-07-06 12:32:40 -0300

    improved the code

--- exercise16.py
+++ exercise16.py
@@ -1,8 +1,13 @@
-from math import floor
+# from math import floor
+
+# n = float(input('Digite um número: '))
+
+# print('O número {} tem a parte inteira {}'.format(n, floor(n)))
 
 n = float(input('Digite um número: '))
 
-print('O número {} tem a parte inteira {}'.format(n, floor(n)))
+print('O número {} tem a parte inteira {}'.format(n, int(n)))
+
 
 # print(emoji.emojize('Olá Mundo :earth_americas:', language='alias'))
 
