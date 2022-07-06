5bdf37297f6bffec9b37f15fe3b806e3c72a7629
Andresa43 <85956453+Andresa43@users.noreply.github.com>
2022-07-06 12:33:58 -0300

    create new code

--- exercise20.py
+++ exercise20.py
@@ -6,4 +6,4 @@ nome2 = str(input('Digite o segundo nome: '))
 nome3 = str(input('Digite o terceiro nome: '))
 nome4 = str(input('Digite o quarto nome: '))
 
-print('Ordem sorteada: {}'.format(random.choices([nome1, nome2, nome3, nome4], k=4)))
+print('Ordem sorteada: {}'.format(random.sample([nome1, nome2, nome3, nome4], 4)))
