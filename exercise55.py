7c740d0f4b71bd8bc4159dfaaf063014d1bc8c7d
Andresa43 <85956453+Andresa43@users.noreply.github.com>
2022-07-19 11:17:43 -0300

    improved the code

--- exercise55.py
+++ exercise55.py
@@ -1,19 +1,17 @@
 
+greater_weight = 0
+less_weight = 0
 
-# num = []
-maior = 0
-menor = 0
+for index in range (0, 5):
+    weight = float(input('Digite o peso da pessoa: '))
 
-for c in range (0, 5):
-    peso = float(input('Digite o peso da pessoa: '))
-    # num.append(peso)
-
-    if c == 0:
-        maior = peso 
-        menor = peso 
+    if index == 0:
+        greater_weight = weight 
+        less_weight = weight 
     else:
-        if peso > maior:
-            maior = peso
-        if menor > peso:
-            menor = peso
-print('Maior {} Menor {}'.format(maior, menor))
+        if weight >  greater_weight:
+            greater_weight = weight
+        if less_weight > weight:
+            less_weight = weight
+
+print('Maior Peso = {} Menor Peso = {}'.format(greater_weight, less_weight))
--- exercise56.py
+++ exercise56.py
@@ -1,12 +1,30 @@
-media = 0
-s = 0
-for c in range (0, 4):
+
+average = 0
+maior = 0 
+saveName = ''
+sumF = 0
+
+for index in range (0, 4):
+    print('------------- {}° Pessoa -------------------'.format(index+1))
     name = str(input('Digite o seu nome: '))
     idade = int(input('Digite a sua idade:  '))
-    media += idade/4
-    sex = str(input('Qual o seu sexo? F/M: ')).upper()
+    average += idade/4
+    sex = str(input('Qual o seu sexo? [F/M:] ')).upper()
+    
     if sex == 'F' and idade < 20:
-        s = c + 1
+        sumF = index + 1
+    
+    if index == 0 and sex == 'M':
+        maior = idade
+        saveName = name
+    else:
+        if idade > maior and sex == 'M':
+            maior = idade
+            saveName = name
+    
 
-print('Media do grupo {}'.format(media))
-print('Mulheres que tem menos de 20 anos é igual a {}'.format(s))
+
+print('Media do grupo é de {} anos'.format(average))
+print('Mulheres que tem menos de 20 anos é igual a {}'.format(sumF))
+print('O homem mais velho tem {} anos e o nome dele é {}'.format(maior, saveName))
+
